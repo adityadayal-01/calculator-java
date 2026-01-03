6e253ad3b4c6c0310739aa9eb85825a1e00e7a10
Aditya dayal <aditya.dayal_cs25@gla.ac.in>
2026-01-03 13:46:27 +0530

    Initial commit: calculator java Choice-driven

--- Calculator.java
+++ Calculator.java
@@ -1,26 +1,40 @@
 import java.util.Scanner;
-class Calculator{
+class calculator{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Entre first number: ");
         float a = sc.nextFloat();
-
         System.out.println("Entre second number: ");
         float b = sc.nextFloat();
-        //for sum
-        float sum = a+b;
-        //for difference
-        float difference = a-b;
-        //for multiplication
-        float product = a*b;
-        //and for division
-        float quotient = a / b;
-        System.out.println("\nResult: ");
-        System.out.println("Addition = " + sum);
-        System.out.println("Substraction = " + difference);
-        System.out.println("Multiplication = " + product);
-        System.out.println("Division = " + quotient);
 
+        System.out.println("\nchoose operation: ");
+        System.out.println("1.Addition");
+        System.out.println("2.Substrsction");
+        System.out.println("3.Multiplication");
+        System.out.println("4.Division");
+
+        System.out.println("Entre your choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Addition= "+(a+b));
+                break;
+            case 2:
+                System.out.println("Substraction= "+(a-b));
+                break;
+            case 3:
+                System.out.println("Multiplication= "+(a*b));
+                break;
+            case 4:
+                if (b!=0)
+                System.out.println("Division="+(a/b));
+                else
+                    System.out.println("Division by zero not allowed");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
         sc.close();
     }
 }
