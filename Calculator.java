5ee8dfcfe9032824ed35da61b88e18002a3ce111
Aditya dayal <aditya.dayal_cs25@gla.ac.in>
2026-01-03 15:32:26 +0530

    Repetable caculator by choice

--- Calculator.java
+++ Calculator.java
@@ -2,39 +2,45 @@ import java.util.Scanner;
 class calculator{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entre first number: ");
-        float a = sc.nextFloat();
-        System.out.println("Entre second number: ");
-        float b = sc.nextFloat();
+        char choice;
+        do {
+            System.out.print("Entre the first number: ");
+            float a = sc.nextFloat();
 
-        System.out.println("\nchoose operation: ");
-        System.out.println("1.Addition");
-        System.out.println("2.Substrsction");
-        System.out.println("3.Multiplication");
-        System.out.println("4.Division");
+            System.out.print("Entre the second number: ");
+            float b = sc.nextFloat();
 
-        System.out.println("Entre your choice: ");
-        int choice = sc.nextInt();
+            System.out.println("\nchoose what operation you prefer: ");
+            System.out.println("1. Addition");
+            System.out.println("2. Substraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.print("Entre your choice: ");
+            int op = sc.nextInt();
 
-        switch (choice) {
-            case 1:
-                System.out.println("Addition= "+(a+b));
-                break;
-            case 2:
-                System.out.println("Substraction= "+(a-b));
-                break;
-            case 3:
-                System.out.println("Multiplication= "+(a*b));
-                break;
-            case 4:
-                if (b!=0)
-                System.out.println("Division="+(a/b));
-                else
-                    System.out.println("Division by zero not allowed");
-                break;
-            default:
-                System.out.println("Invalid choice");
-        }
+            switch (op) {
+                case 1:
+                    System.out.println("Addittion: " + (a + b));
+                    break;
+                case 2:
+                    System.out.println("Substraction: " + (a - b));
+                    break;
+                case 3:
+                    System.out.println("Multiplication: " + (a * b));
+                    break;
+                case 4:
+                    if (b != 0)
+                        System.out.println("Division: " + (a / b));
+                    else
+                        System.out.println("Division by zero not allowed");
+                default:
+                    System.out.println("Invalid choice");
+            }
+            System.out.println("\nDo you want to continue? (y/n): ");
+            choice = sc.next().charAt(0);
+            System.out.println();
+        } while (choice == 'y' || choice == 'y');
         sc.close();
+        System.out.println("Calculator closed. Thank you");
     }
 }
